37b4e42aa9a5a712278730ed36b5bb3ade39d05d
Camellia <2043248113@qq.com>
2026-04-20 17:06:06 +0800

    fix waline

--- src/site.config.ts
+++ src/site.config.ts
@@ -174,7 +174,7 @@ export const integ: IntegrationUserConfig = {
         reaction0: 'Like',
         placeholder: 'Welcome to comment. (Email to receive replies. Login is unnecessary)'
       },
-      imageUploader: false
+      imageUploader: undefined,
     }
   }
 }
